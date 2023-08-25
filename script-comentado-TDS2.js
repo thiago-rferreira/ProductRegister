21e5e307b75842a2c65d928f097175dfae740355
thiagoferreira564 <thiago.rodrigues@sp.senai.br>
2023-08-25 14:19:37 -0300

    TDS1-comentado

--- script-comentado-TDS2.js
+++ script-comentado-TDS2.js
@@ -18,33 +18,33 @@ function verificarInputs() {
 
     // Vamos dar um console ou verbo consolar para cada variável.
     console.log("Titulo" + titulo);
-    console.log({preco});
-    console.log({descricao});
-    console.log({plataforma});
-    console.log({imgLink});
+    console.log({ preco });
+    console.log({ descricao });
+    console.log({ plataforma });
+    console.log({ imgLink });
 
     // Vamos entao criar uma verificacao se os inputs estao vazios.
     // Pois se estao vazio eu preciso de um alerta para o usuario.
     // ou uma mensagem de erro.
     // Para isso, vamos criar uma condicional if.
-    if(titulo == ""|| preco == "" || descricao == "" || plataforma == "" || imgLink == ""){
+    if (titulo == "" || preco == "" || descricao == "" || plataforma == "" || imgLink == "") {
         // Verificar se o if esta funcionando
         console.log("Os dados estao vazios");
         // Como esse é o primeiro if de verificacao, vamos criar 
         // uma funcao que envia/coloca na tela a mensagem de erro.
         // Usando a div que já temos com id msg.
-        return true;     
-    }else{
+        return true;
+    } else {
         console.log("Os dados nao estao em branco");
         return false;
     }
 }
 
-function envieMsg(msg, tipo){
+function envieMsg(msg, tipo) {
     //Essa funcao vai colocar uma msg que vem parametro na tela 
     // do usuario. Logo a primeira coisa que eu tenho que fazer
     // é usar as tecnicas de HMTL com JS.
-   
+
     let msgDiv = document.getElementById("msg");
     msgDiv.innerHTML = "";
 
@@ -56,7 +56,7 @@ function envieMsg(msg, tipo){
     // O timeout ocorre depois que o tempo passado para ele
     // E executa o que esta dentro da function, no nosso caso
     // ele limpa a msgDiv para tirar da tela depois de 3000ms ou 3s.
-    setTimeout(function(){
+    setTimeout(function () {
         msgDiv.innerHTML = "";
     }, 3000);
 }
@@ -67,12 +67,12 @@ function envieMsg(msg, tipo){
 // Ou seja, se estou trabalhando com Jogos, agora eu crio a Jogo.
 
 //Sintaxe da class
-class Jogo{
+class Jogo {
     // Apos criar a class, eu preciso criar o meu construtor.
     // E nele, eu irei passar todas as propriedados, exceto: array e 
     // propriedades que serao calculadas por alguma funcao/metodo que eu 
     // desenvolva.
-    constructor(titulo, preco, descricao, plataforma, imgLink){
+    constructor(titulo, preco, descricao, plataforma, imgLink) {
         // fazer as referencias das variaveis, utilizando o this.
         // e aqui que irá aparecer tbm os: arrays(se precisar)
         // e as propriedades que serao calculadas com alguma funcao/metodo.
@@ -90,7 +90,7 @@ class Jogo{
 // Criar um objeto com o tipo dele e uma variavel do nome que eu preferir.
 // sempre const.
 
-const jogoTeste = new Jogo("Teste", "12", "Desc","steam","link");
+const jogoTeste = new Jogo("Teste", "12", "Desc", "steam", "link");
 // Apos criar esse objeto, eu dou um console para verificar
 // Se ele esta sendo montado.
 console.log(jogoTeste)
@@ -100,7 +100,7 @@ console.log(jogoTeste)
 // Eu pego os valores dos inputs e instancio um objeto do tipo
 // jogo, porém eu tenho que ser generico.
 
-function comporJogo(){
+function comporJogo() {
     // Para compor meu JOGO eu preciso pegar os valores dos inputs.
     // Para isso, eu preciso criar uma variavel para cada input.
 
@@ -113,7 +113,7 @@ function comporJogo(){
     // Agora eu preciso instanciar/montar um objeto do tipo Jogo.
     // Só que ao inves de digitar os valores manualmente, eu vou
     // pegar os valores das variaveis.
-    const jogo = new Jogo(titulo,preco,descricao,plataforma,imgLink);
+    const jogo = new Jogo(titulo, preco, descricao, plataforma, imgLink);
 
     //Agora vou testar se meu objeto esta pegando os valores do input e sendo
     //montado
@@ -133,12 +133,12 @@ function comporJogo(){
     renderizarConteudo();
 }
 
-class ListaJogos{
+class ListaJogos {
     // Apos criar a class, eu preciso criar o meu construtor.
     // E nele, eu irei passar todas as propriedados, exceto: array e 
     // propriedades que serao calculadas por alguma funcao/metodo que eu 
     // desenvolva.
-    constructor(){
+    constructor() {
         // Como será uma lista, eu preciso criar um this.listaQualquer
         // Com um array vazio.
         this.listaJogosArray = [];
@@ -153,7 +153,7 @@ class ListaJogos{
 
     // Quando eu vou adicionar algo, eu preciso saber o que, logo
     // eu uso parametro.
-    add(parametro){
+    add(parametro) {
         // Entao precisamos pegar o jogo que foi composto lá na linha 117.
         // E dar um push no array que foi criado na linha 136.
         // this.listaJogos.push(parametro);
@@ -167,12 +167,12 @@ class ListaJogos{
 
         //Aqui faremos as verificacoes, antes de adicionar jogo, dentro
         // do array, usando as funcoes que temos de acoes.
-        if(verificarInputs()){
+        if (verificarInputs()) {
             envieMsg("Preencha todos os campos", "erro");
-        }else{
+        } else {
             this.listaJogosArray.push(parametro);
             limparInputs();
-            envieMsg("Cadastrado com sucesso","sucesso")
+            envieMsg("Cadastrado com sucesso", "sucesso")
             console.log(this.listaJogosArray);
         }
     }
@@ -187,7 +187,7 @@ console.log(bibliotecaJogos);
 
 // Criar uma funcao que nos auxilia, e limpa todos os campos para mim
 // Uso ela sempre que precisar.
-function limparInputs(){
+function limparInputs() {
     console.log("Usei a funcao de limparInputs");
 
     //Pego o valor dela, e defino como vazio.
@@ -204,7 +204,7 @@ function limparInputs(){
 // Entao, eu crio uma funcao que fica responsavel por colocar/renderizar
 // os itens de dentro do meu objeto na tela.
 
-function renderizarConteudo(){
+function renderizarConteudo() {
     // Variavel para armazenar o innerHMTL, que é
     // responsavel por renderizar na tela
 
@@ -227,7 +227,7 @@ function renderizarConteudo(){
         //crio uma div em html, dentro do JS mesmo.
         // Que vai ser colocada dentro da div, que esta esperando
         // no html.
-       const jogoDiv = `
+        const jogoDiv = `
             <!--
                 Comentario:
                 Crio a div, com class para usar depois no CSS
@@ -243,6 +243,6 @@ function renderizarConteudo(){
             </div>
        `;
 
-       listaHTML.innerHTML += jogoDiv;
+        listaHTML.innerHTML += jogoDiv;
     });
 }
